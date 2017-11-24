c8319d78da143a9a7f5780baf2fc0e568688c97c
Vadym Ulianov <vadymulianov@gmail.com>
2017-11-24 21:59:07 +0100

    Changes in onClick

--- designs.js
+++ designs.js
@@ -37,16 +37,31 @@ $(function makeGrid() {
   // Getting Color Picker Value Function
   $("#colorPicker").on("change", function() {
     colorPickerValue = $("#colorPicker").val();
+    console.log("colorPickerValue:", colorPickerValue);
   });
+
+  // Converting hex value of colorPickerValue to rgb value
+  function rgba(hex, opacity) {
+  var colours = hex.regex(/#?([0-9A-F]{2})([0-9A-F]{2})([0-9A-F]{2})/i).map(function(val) {
+    return Math.range(0, parseInt(val, 16), 255);
+  }).join(",");
+  if (opacity === undefined) {
+    return "rgb(" + colours + ")";
+  }
+  return "rgba(" + colours + "," + opacity + ")";
+  }
+
+  // Converted colorPickerValue
+  var colorPickerValueConverted = rgba(colorPickerValueConverted);
+  console.log("colorPickerValueConverted:", colorPickerValueConverted);
 
   // Pixel drawning Function
   $("#pixel_canvas").on("click", "td", function() {
+    if ($(this).css("background-color") == "rgba(0, 0, 0, 0)") {
       $(this).css("background-color", colorPickerValue);
-  });
-
-  // Pixel removing function on doubleClick
-  $("#pixel_canvas").on("dblclick", "td", function() {
-    $(this).css("background-color", "white");
+    } else {
+      $(this).css("background-color", "rgba(0,0,0,0)");
+    }
   });
 
 });
